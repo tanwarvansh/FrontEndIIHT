8ee8a61f7efd8c86bc22d055defbe5d5b00a367c
Vansh Tanwar <vanshtanwar9@gmail.com>
2022-12-06 11:39:59 +0530

    BasicJsAssignmentAdded

---
+++ December6th/JS/app.js
@@ -0,0 +1,44 @@
+const employee ={
+    employeeId:212121,
+    name:"Abhishek Kumar",
+    department:{
+        id:213,
+        name:"ItSupport",
+        mgrId:123
+    },
+    salary:20000,
+    dateOfJoining:new Date("2019-03-25"),
+    getNumberOfServiceYears:function(){
+        return (2022-this.dateOfJoining.getFullYear());
+    }
+
+
+}
+
+
+console.log(employee);
+
+
+console.log(employee.employeeId);
+console.log(employee.name);
+console.log(employee.department.id);
+console.log(employee.department.name);
+console.log(employee.salary);
+console.log(employee.dateOfJoining);
+console.log(employee.getNumberOfServiceYears());
+
+// console.clear();
+
+const employees=[
+    {id:21,name:"Vansh",age:22},
+    {id:22,name:"Akshay",age:22},
+    {id:22,name:"Abhishek",age:22}
+];
+
+
+for(let i=0;i<employees.length;i++){
+    console.log(employees[i]);
+}
+
+
+
